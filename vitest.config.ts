00ac8f156b2fac695e8a5897432919a008c553a5
Alexandre Skinner <AlexandreSkinner@tmassessoria.com.br>
2023-11-15 12:54:12 -0300

    chore: adiciona include vitest.config.ts no tesconfig.json e fix regras eslint

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vitest/config";
-import path from "path";
+import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
+    environment: 'node'
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@/test": path.resolve(__dirname, "./test"),
-    },
-  },
+      '@': path.resolve(__dirname, './src'),
+      '@/test': path.resolve(__dirname, './test')
+    }
+  }
 });
