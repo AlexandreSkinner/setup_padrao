3259203302ece9191390027e95204ce918314b63
Alexandre Skinner <AlexandreSkinner@tmassessoria.com.br>
2024-01-13 18:26:06 -0300

    chore: add the dts key to the tsup.config file

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
+  target: 'es2022',
   entry: ['src/index.ts'],
   splitting: false,
   sourcemap: true,
-  clean: true
+  clean: true,
+  dts: true
 });
